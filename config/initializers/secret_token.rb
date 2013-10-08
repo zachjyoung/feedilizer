61c748c5c8dd0530cec8d8f558cede37f63a7f9f
Zach Young <zachjyoung@gmail.com>
2013-10-08 13:38:11 -0400

    fixed config initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Feedilizer::Application.config.secret_key_base = '52d02b6d11d7492d0742564956b78fe4b1e301ef5ef412e77fe7dcd0f97ce5dceba5491e643571aa238b213f767db0f8100efad39637da7b883cb02205439218'
