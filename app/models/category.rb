a51a774ae977a43811d8faac013ad95e002547c6
Kyle Hutchinson <mskylehutchinson@gmail.com>
2013-10-10 17:14:07 -0400

    Updated the model
    Added join table between user and blog
    Added user_id to category
    Added validations to models
    Added associations to models
    Added tests for all that business

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,3 +1,7 @@
 class Category < ActiveRecord::Base
   has_many :blogs, through: :blog_categorizations, inverse_of: :categories
+  belongs_to :user, inverse_of: :categories
+
+  validates_presence_of :user
+  validates_presence_of :name
 end
--- spec/factories/categories.rb
+++ spec/factories/categories.rb
@@ -1,5 +1,6 @@
 FactoryGirl.define do
   factory :category do
     sequence(:name) { |n| "The awesome blog ##{n}" }
+    user User.new
   end
 end
