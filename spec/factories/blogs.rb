c4e89a18e532e2abc78af781d7e9489651e52e85
Kyle Hutchinson <mskylehutchinson@gmail.com>
2013-10-16 12:27:17 -0400

    refactored user_sees_user_feed spec

--- spec/factories/blogs.rb
+++ spec/factories/blogs.rb
@@ -5,5 +5,15 @@ FactoryGirl.define do
     sequence(:url) { |n| "http://www.nytimes.com/services/xml/rss/nyt/#{n}JobMarket.html" }
     title "NYT &gt; Jobs"
     feed_url "http://www.nytimes.com/services/xml/rss/nyt/JobMarket.xml"
+
+    factory :blog_with_entries do
+      ignore do
+        blog_entries_count 5
+      end
+
+      after(:create) do |blog, evaluator|
+        FactoryGirl.create_list(:blog_entry, evaluator.blog_entries_count, blog: blog)
+      end
+    end
   end
 end
--- spec/features/user_sees_user_feed.rb
+++ spec/features/user_sees_user_feed.rb
@@ -12,12 +12,9 @@ feature "user sees user's own feed", %Q{
 
   let!(:user) { FactoryGirl.create(:user) }
   let!(:user2) { FactoryGirl.create(:user) }
-  let!(:blog) { FactoryGirl.create(:blog) }
-  let!(:blog2) { FactoryGirl.create(:blog) }
-  let!(:blog3) { FactoryGirl.create(:blog) }
-  let!(:blog_entry) { FactoryGirl.create(:blog_entry, blog: blog) }
-  let!(:blog_entry2) { FactoryGirl.create(:blog_entry, blog: blog2) }
-  let!(:blog_entry3) { FactoryGirl.create(:blog_entry, blog: blog3) }
+  let!(:blog) { FactoryGirl.create(:blog_with_entries) }
+  let!(:blog2) { FactoryGirl.create(:blog_with_entries) }
+  let!(:blog3) { FactoryGirl.create(:blog_with_entries) }
   let!(:user_blog) { FactoryGirl.create(:user_blog, blog: blog, user: user) }
   let!(:user_blog2) { FactoryGirl.create(:user_blog, blog: blog2, user: user2) }
   let!(:user_blog3) { FactoryGirl.create(:user_blog, blog: blog3, user: user) }
@@ -26,7 +23,7 @@ feature "user sees user's own feed", %Q{
   scenario 'user views her feed' do
     login_user(user)
     visit blog_entries_path
-    expect(page).to have_content(blog_entry.name, blog_entry3.name)
-    expect(page).to have_no_content(blog_entry2.name)
+    expect(page).to have_content(blog.blog_entries.first.name, blog3.blog_entries.first.name)
+    expect(page).to have_no_content(blog2.blog_entries.first.name)
   end
 end
